3a897639f2ab7c84d5f98c383257a5d5da8fe2ca
felicianpopa <felician.popa@bootintech.com>
2025-03-25 16:06:48 +0200

    SearchParams

--- src/app/articles/[articleId]/page.tsx
+++ src/app/articles/[articleId]/page.tsx
@@ -1,16 +1,19 @@
 import Link from "next/link";
 
-const Home = () => {
+const NewsArticle = async ({
+  params,
+  searchParams,
+}: {
+  params: Promise<{ articleId: string }>;
+  searchParams: Promise<{ lang: string }>;
+}) => {
+  const { articleId } = await params;
+  const { lang } = await searchParams;
   return (
-    <>
-      <h1>Home Page</h1>
+    <div>
+      <h1>News article {articleId}</h1>
+      <p>Reading in {lang}</p>
       <ul className="flex space-x-4 text-blue-400">
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/products">Products</Link>
-        </li>
         <li>
           <Link href="/articles/breaking-news-123?lang=en">
             Read in English
@@ -20,8 +23,8 @@ const Home = () => {
           <Link href="/articles/breaking-news-123?lang=fr">Read in French</Link>
         </li>
       </ul>
-    </>
+    </div>
   );
 };
 
-export default Home;
+export default NewsArticle;
