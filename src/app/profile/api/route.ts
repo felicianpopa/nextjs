7a7678fe1200910dea79c906311e7bcbe5d94bd2
felicianpopa <felician.popa@bootintech.com>
2025-04-03 16:24:42 +0300

    Cookies

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,13 +1,19 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET() {
   const headerList = await headers();
   console.warn(headerList.get("Authorization"));
+
+  const cookieStore = await cookies();
+  cookieStore.set("resultsPerPage", "20");
+
+  console.warn("cookies ", cookieStore.get("theme"));
   return new Response(
     "<h1>Profile API! stored in /api not to override the page.tsx</h1>",
     {
       headers: {
         "Content-Type": "text/html",
+        "Set-Cookie": "theme=dark",
       },
     }
   );
