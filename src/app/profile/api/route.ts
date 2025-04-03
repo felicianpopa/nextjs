8a5e8413fb115e74b37cfe601af72900921d8204
felicianpopa <felician.popa@bootintech.com>
2025-04-03 16:15:23 +0300

    Headers in route handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,5 +1,14 @@
+import { headers } from "next/headers";
+
 export async function GET() {
+  const headerList = await headers();
+  console.warn(headerList.get("Authorization"));
   return new Response(
-    "Profile API! stored in /api not to override the page.tsx"
+    "<h1>Profile API! stored in /api not to override the page.tsx</h1>",
+    {
+      headers: {
+        "Content-Type": "text/html",
+      },
+    }
   );
 }
