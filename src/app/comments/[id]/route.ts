2826f7b77091030ae7c2f9816cd8fb8017c7a2c2
felicianpopa <felician.popa@bootintech.com>
2025-04-03 09:01:39 +0300

    PATCH

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -8,3 +8,14 @@ export async function GET(
   const comment = comments.find((comment) => comment.id === Number(id));
   return Response.json(comment);
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: number } }
+) {
+  const { id } = await params;
+  const { text } = await request.json();
+  const index = comments.findIndex((comment) => comment.id === Number(id));
+  comments[index].text = text;
+  return Response.json(comments[index]);
+}
