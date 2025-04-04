6287d515159e8a27c480518ec28e4c836b7f93c1
felicianpopa <felician.popa@bootintech.com>
2025-04-04 11:30:14 +0300

    Server and client components

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { useState } from "react";
+
+const DashboardPage = () => {
+  const [name, setName] = useState("");
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <p>Hello, {name}</p>
+    </div>
+  );
+};
+
+export default DashboardPage;
