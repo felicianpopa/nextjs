93248608621880a552b4ec6911abccacbfd14074
felicianpopa <felician.popa@bootintech.com>
2025-04-04 17:20:12 +0300

    Context Providers

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -1,10 +1,18 @@
 "use client";
 
+import { useTheme } from "@/components/theme-provider";
+
 import React from "react";
 import ImageSlider from "@/components/ImageSlider";
 
 const ClientRoutePage = () => {
-  return <ImageSlider />;
+  const theme = useTheme();
+  return (
+    <>
+      <ImageSlider />
+      <h1 style={{ color: theme.colors.primary }}>Theme </h1>
+    </>
+  );
 };
 
 export default ClientRoutePage;
