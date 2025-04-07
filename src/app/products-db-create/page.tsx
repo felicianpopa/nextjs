ef35df789624c8c687552cc9808d50da0665fbb2
felicianpopa <felician.popa@bootintech.com>
2025-04-07 15:17:49 +0300

    Pending state

--- src/app/products-db-create/page.tsx
+++ src/app/products-db-create/page.tsx
@@ -1,3 +1,4 @@
+import Submit from "@/components/submit";
 import { addProduct } from "@/prisma-db";
 import { redirect } from "next/navigation";
 const AddProductPage = () => {
@@ -34,12 +35,7 @@ const AddProductPage = () => {
           name="description"
         />
       </label>
-      <button
-        type="submit"
-        className="block w-full p-2 text-black bg-blue-500 rounded disabled:bg-gray-500"
-      >
-        Add product
-      </button>
+      <Submit />
     </form>
   );
 };
