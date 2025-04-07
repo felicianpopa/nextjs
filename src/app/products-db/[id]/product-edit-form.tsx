b96bdf3da709fb1a93511c6787b45c9c3a8bf4a6
felicianpopa <felician.popa@bootintech.com>
2025-04-07 16:23:57 +0300

    Update Server action

---
+++ src/app/products-db/[id]/product-edit-form.tsx
@@ -0,0 +1,52 @@
+"use client";
+import Submit from "@/components/submit";
+import { useActionState } from "react";
+import { FormState, editProduct } from "@/actions/products";
+import { Product } from "../page";
+
+const EditProductForm = ({ product }: { product: Product }) => {
+  const initialState: FormState = {
+    errors: {},
+  };
+
+  const editProductWithId = editProduct.bind(null, product.id);
+
+  const [state, formAction] = useActionState(editProductWithId, initialState);
+
+  return (
+    <form action={formAction} className="p-4 space-y-4 max-w-96">
+      <label className="text-black">
+        Title
+        <input
+          type="text"
+          className="block w-full p-2 text-black border rounded"
+          name="title"
+          defaultValue={product.title}
+        />
+      </label>
+      {state.errors.title && <p>{state.errors.title}</p>}
+      <label className="text-black">
+        Price
+        <input
+          type="number"
+          className="block w-full p-2 text-black border rounded"
+          name="price"
+          defaultValue={product.price}
+        />
+      </label>
+      {state.errors.price && <p>{state.errors.price}</p>}
+      <label className="text-black">
+        Description
+        <textarea
+          className="block w-full p-2 text-black border rounded"
+          name="description"
+          defaultValue={product.description ?? ""}
+        />
+      </label>
+      {state.errors.description && <p>{state.errors.description}</p>}
+      <Submit />
+    </form>
+  );
+};
+
+export default EditProductForm;
