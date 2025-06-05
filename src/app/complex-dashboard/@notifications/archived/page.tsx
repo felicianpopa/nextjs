371d919e7cfcd5245a540cae0d27f90bf62e37e3
felicianpopa <felician.popa@bootintech.com>
2025-06-05 07:13:18 +0300

    Parallel routes update

--- src/app/complex-dashboard/@notifications/archived/page.tsx
+++ src/app/complex-dashboard/@notifications/archived/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Archived = () => {
   return (
     <Card>
-      <div>Archived</div>
+      <div>Archived page</div>
       <div>
         <Link
           className="ext-blue-500 hover:underline text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300"
